57e1eb4334ff8302f572a5397773b45a54b768d9
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-05-08 21:54:38 -0500

    Se generó boilerplate con React

---
+++ burgerqueen/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Esta es una prueba sin gatito <br /> necesitamos gatitos u_u <br /> Shalom, shalom and thanks for all the fish
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
